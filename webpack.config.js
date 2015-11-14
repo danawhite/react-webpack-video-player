62b81fa769ef1d152ab38cfd79ef1e66acce3633
danawhite <danatroymedia@gmail.com>
2015-11-14 00:38:24 -0500

    adding webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,45 @@
 var webpack = require('webpack');
+var merge = require('webpack-merge');
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin');
 
-const APP_PATH = __dirname + 'src/app';
-const BUILD_PATH = __dirname + 'src/build'; 
+const ROOT_PATH = path.resolve(__dirname, 'src');
+const APP_PATH = path.resolve(ROOT_PATH, 'app');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+const TARGET = process.env.BABEL_ENV = process.env.npm_lifecycle_event;
 
-module.exports = {
-        
+var common = {
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    entry: APP_PATH + '/app.jsx',
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'React Video Player'
+        }),
+        new OpenBrowserWebpackPlugin({ url: 'http://localhost:8080'}),
+    ],
+    module: {
+        loaders: [
+            { test: /\.css$/, include: APP_PATH, loader: 'style!css'},
+            { test: /\.jsx?$/, include: APP_PATH, loader: 'babel'}
+        ]
+    }
 };
+
+// provides default in the event that we're running Webpack outside of npm
+if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+
